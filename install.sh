53fac3ea7dcfcb0431df214057c7838723f23140
Sunny Bharne <sunny.bharne@outlook.com>
2025-02-22 16:39:56 +0000

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
-# Create code dir
-mkdir -p ~/code && cd ~/code
+# Check if the 'code' directory exists, and create it if it doesn't
+CODE_DIR="$HOME/code"
+if [ ! -d "$CODE_DIR" ]; then
+    echo "Directory $CODE_DIR does not exist. Creating it..."
+    mkdir -p "$CODE_DIR"
+else
+    echo "Directory $CODE_DIR already exists."
+fi
 
-# Remove the dotfiles repo
-rm -rf dotfiles
+# Check if the .gitconfig symlink exists, and remove it if it does
+GITCONFIG_SYMLINK="$HOME/.gitconfig"
+if [ -L "$GITCONFIG_SYMLINK" ]; then
+    echo "Symlink $GITCONFIG_SYMLINK exists. Removing it..."
+    rm "$GITCONFIG_SYMLINK"
+else
+    echo "Symlink $GITCONFIG_SYMLINK does not exist."
+fi
 
-# Clone the dotfiles repo
-git clone https://github.com/sunnybharne/dotfiles.git
-
-# Remove .files from home
-rm ~/.gitconfig
-
-# create links to the dot files
-ln -s .gitconfig ~/.gitconfig
+# Check if the .zshrc symlink exists, and remove it if it does
+ZSHRC_SYMLINK="$HOME/.zshrc"
+if [ -L "$ZSHRC_SYMLINK" ]; then
+    echo "Symlink $ZSHRC_SYMLINK exists. Removing it..."
+    rm "$ZSHRC_SYMLINK"
+else
+    echo "Symlink $ZSHRC_SYMLINK does not exist."
+fi
 
