d7783fcbb3c68c5a1f295e16b082d3a21c01e555
Sunny Bharne <sunny.bharne@outlook.com>
2025-02-22 15:15:40 +0000

    Updated install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,9 @@ rm -rf dotfiles
 # Clone the dotfiles repo
 git clone https://github.com/sunnybharne/dotfiles.git
 
+# Remove .files from home
+rm -rf ~/.gitconfig
+
 # create links to the dot files
 ln -s .gitconfig ~/.gitconfig
 
