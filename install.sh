b667ae5393cb6b5e61e72a758e2d465d7bf7cf80
Sunny Bharne <sunny.bharne@outlook.com>
2025-02-22 14:39:49 +0000

    Change repo name to dotfiles

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@
 mkdir -p "$HOME/code"
 
 # Clone the dotfiles repo
-Git clone https://github.com/sunnybharne/.dotfiles.git ~/code/.dotfiles
+Git clone https://github.com/sunnybharne/dotfiles.git ~/code/dotfiles
